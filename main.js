7dcaae295a27f02b57847e3975a111bb0ace1422
MahmudNazarov <sh.nazarov@gmail.com>
2024-12-03 15:17:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// console.log(12);
+// console.log(100);
+// console.log("Murod" + "Hayot");
+
+// var ism="mahmud"
+// console.log(ism);
+
+// var ism = prompt("ismingizni kriting")
+var familiya = prompt("familiyaningizni kriting")
+var yosh = +prompt("yoshingiz netchida")
+console.log(yosh,familiya);
